049d30789309cc2f6c3fb58fe0c9d436fab21b45
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-07-25 13:09:22 -0400

    updates

--- main.js
+++ main.js
@@ -39,3 +39,26 @@ window.onscroll = () =>
     menuIcon.classList.remove('bx-grid-vertical');
     navbar.classList.remove('active');
 };
+// scroll reveal js
+ScrollReveal
+({
+    reset: true,
+    distance: '80px',
+    duration: 1700,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img, .experience-container, .portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right'});
+
+// span scroling using typed js
+ const typed = new Typed('.multi-text',
+{
+    strings:['Software Developer','Machine Learning Expert','Game Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
