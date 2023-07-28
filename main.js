b3600b67be79d03dd0fc7996e3d2f1bb425ba860
Carl Hartry Jr <carlhartry+github@gmail.com>
2023-07-28 14:23:22 -0400

    update

--- main.js
+++ main.js
@@ -50,8 +50,8 @@ ScrollReveal
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
 ScrollReveal().reveal('.home-img, .experience-container, .portfolio-box, .contact form', { origin: 'bottom'});
-ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left'});
-ScrollReveal().reveal('.home-content p, .about-content ', { origin: 'right'});
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 // span scroling using typed js
  const typed = new Typed('.multi-text',
